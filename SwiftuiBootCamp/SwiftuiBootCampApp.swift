cb8146367c272830cf3480badfee8c4b83659350
ba138 <basitalyshah51214@gmail.com>
2026-05-06 20:40:51 +0500

    Initial Commit

---
+++ SwiftuiBootCamp/SwiftuiBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiBootCampApp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 06/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

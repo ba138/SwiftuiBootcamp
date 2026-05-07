885439abc3eefa50db2e7c81ccca4095de521a75
ba138 <basitalyshah51214@gmail.com>
2026-05-07 11:33:51 +0500

    adding text and it modifiers to text

---
+++ SwiftuiBootCamp/textbootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  textbootcamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 07/05/2026.
+//
+
+import SwiftUI
+
+struct textbootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    textbootcamp()
+}

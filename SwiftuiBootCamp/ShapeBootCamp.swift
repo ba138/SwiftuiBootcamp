0076cb1c5578acb4057a41eef17dac80e593f87e
ba138 <basitalyshah51214@gmail.com>
2026-05-07 20:25:17 +0500

    Adding the capusle shape

--- SwiftuiBootCamp/ShapeBootCamp.swift
+++ SwiftuiBootCamp/ShapeBootCamp.swift
@@ -16,7 +16,8 @@ struct ShapeBootCamp: View {
 //            .stroke(Color.red,lineWidth: 30).frame(width: 300)
 //            .stroke(Color.red,style: StrokeStyle(lineWidth: 10,lineCap: .butt,dash: [5],),)
 //            .trim(from: 0.2,to: 1.0).stroke(Color.purple ,lineWidth: 10).frame(width: 300)
-        Ellipse().trim().trim(from:0.3,to: 1).fill(Color.blue).frame(width: 300,height: 400)
+//          Ellipse().trim().trim(from:0.3,to: 1).fill(Color.blue).frame(width: 300,height: 400)
+        Capsule(style: .circular).stroke(lineWidth: 10).frame(width: 300,height: 500)
     
         
     }

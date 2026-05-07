ee7a206aca7bfbcb219621733dfee17b435c7384
ba138 <basitalyshah51214@gmail.com>
2026-05-07 19:52:18 +0500

    added shape file

---
+++ SwiftuiBootCamp/ShapeBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 07/05/2026.
+//
+
+import SwiftUI
+
+struct ShapeBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeBootCamp()
+}

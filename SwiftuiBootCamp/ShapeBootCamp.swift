0c1ac9ec6cd4d0da74b57df1bd2e30494da4d68b
ba138 <basitalyshah51214@gmail.com>
2026-05-07 20:39:04 +0500

    Adding rounded rectangle shape and its modifiers

--- SwiftuiBootCamp/ShapeBootCamp.swift
+++ SwiftuiBootCamp/ShapeBootCamp.swift
@@ -17,8 +17,15 @@ struct ShapeBootCamp: View {
 //            .stroke(Color.red,style: StrokeStyle(lineWidth: 10,lineCap: .butt,dash: [5],),)
 //            .trim(from: 0.2,to: 1.0).stroke(Color.purple ,lineWidth: 10).frame(width: 300)
 //          Ellipse().trim().trim(from:0.3,to: 1).fill(Color.blue).frame(width: 300,height: 400)
-        Capsule(style: .circular).stroke(lineWidth: 10).frame(width: 300,height: 500)
-    
+//        Capsule(style: .circular).stroke(lineWidth: 10).frame(width: 300,height: 500)
+//        Rectangle()
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.green)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.red, style: StrokeStyle(lineWidth: 2))
+            )
+            .frame(width: 300, height: 400)
         
     }
 }

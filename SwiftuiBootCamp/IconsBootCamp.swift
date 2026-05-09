d085d7def8b2b3d37ba8fdc12ab55de9286555b1
ba138 <basitalyshah51214@gmail.com>
2026-05-09 10:16:28 +0500

    Adding icon to swiftui

---
+++ SwiftuiBootCamp/IconsBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  IconsBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 09/05/2026.
+//
+
+import SwiftUI
+
+struct IconsBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconsBootCamp()
+}

125b6f31dbf20ac591b1cdd5ac004a948de1a920
ba138 <basitalyshah51214@gmail.com>
2026-05-10 11:39:35 +0500

    Adding image to the image boot camp

---
+++ SwiftuiBootCamp/ImageBootCamp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 10/05/2026.
+//
+
+import SwiftUI
+
+struct ImageBootCamp: View {
+    var body: some View {
+        Image("google").resizable()    }
+}
+
+#Preview {
+    ImageBootCamp()
+}

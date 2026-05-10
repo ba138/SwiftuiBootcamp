d2cbd66f4b0f1a4bef82b0c34ab5f07dc1283f0a
ba138 <basitalyshah51214@gmail.com>
2026-05-10 20:23:14 +0500

    Adding frame boot camp

---
+++ SwiftuiBootCamp/FrameBootCamp.swift
@@ -0,0 +1,21 @@
+//
+//  FrameBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 10/05/2026.
+//
+
+import SwiftUI
+
+struct FrameBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.red)
+            .frame(width: 300,height: 300)
+            .background(Color.green)
+    }
+}
+
+#Preview {
+    FrameBootCamp()
+}

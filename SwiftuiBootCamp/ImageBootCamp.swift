b40dcbb09c9afceb0410b7edcece4b0dec891a17
ba138 <basitalyshah51214@gmail.com>
2026-05-10 12:06:09 +0500

    Adding image icon to image boot camp

--- SwiftuiBootCamp/ImageBootCamp.swift
+++ SwiftuiBootCamp/ImageBootCamp.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct ImageBootCamp: View {
     var body: some View {
-        Image("google").resizable()    }
+        //Adding images
+        //  Image("google")
+        //         .resizable()
+        //            .aspectRatio(contentMode: .fill)
+        //            .scaledToFill()
+        //          .scaledToFit()
+        //           .frame(width: 300, height: 200)
+        //            .clipped()
+        //            .cornerRadius(30)
+        //           .clipShape(
+        //                Circle()
+        //                RoundedRectangle(cornerRadius: 35)
+        //               Ellipse()
+        //           )
+        //Adding image to icon
+        Image("google")
+            .renderingMode(
+                .template)
+            .resizable()
+            .foregroundColor(
+                .red)
+            .scaledToFit()
+            .frame(width: 200,height: 150)
+    }
 }
 
 #Preview {

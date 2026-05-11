82c92f95beb3b66558e89e7838f8148321f1b760
ba138 <basitalyshah51214@gmail.com>
2026-05-11 10:57:33 +0500

    Adding shapes to frames

--- SwiftuiBootCamp/BackgroundAndOverlayBootCamp.swift
+++ SwiftuiBootCamp/BackgroundAndOverlayBootCamp.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct BackgroundAndOverlayBootCamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).background(
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .frame(width: 100,height: 100)
+            .background(
 //            Color.red
-            LinearGradient(colors: [AppColors.button,AppColors.favorite], startPoint: UnitPoint.leading, endPoint: UnitPoint.trailing)
+//            LinearGradient(colors: [AppColors.button,AppColors.favorite], startPoint: UnitPoint.leading, endPoint: UnitPoint.trailing)
+                Circle().fill(Color.green)
         )
+            .background(
+                Circle().fill(Color.red).frame(width: 120,height: 120)
+            )
     }
 }
 

a6549a6274266a7bbce4585bed99d2ab62db7c9c
ba138 <basitalyshah51214@gmail.com>
2026-05-11 11:08:15 +0500

    Adding overlay and develop a button

--- SwiftuiBootCamp/BackgroundAndOverlayBootCamp.swift
+++ SwiftuiBootCamp/BackgroundAndOverlayBootCamp.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct BackgroundAndOverlayBootCamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .frame(width: 100,height: 100)
-            .background(
-//            Color.red
-//            LinearGradient(colors: [AppColors.button,AppColors.favorite], startPoint: UnitPoint.leading, endPoint: UnitPoint.trailing)
-                Circle().fill(
-                              LinearGradient(colors: [Color.red,Color.green], startPoint: UnitPoint.leading, endPoint: .trailing)
-                             )
-        )
-            .background(
-                Circle().fill(
-                    LinearGradient(colors: [Color.blue,Color.green], startPoint:.leading, endPoint:.trailing)
-                ).frame(width: 120,height: 120)
-            )
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .frame(width: 100,height: 100)
+//            .background(
+////            Color.red
+////            LinearGradient(colors: [AppColors.button,AppColors.favorite], startPoint: UnitPoint.leading, endPoint: UnitPoint.trailing)
+//                Circle().fill(
+//                              LinearGradient(colors: [Color.red,Color.green], startPoint: UnitPoint.leading, endPoint: .trailing)
+//                             )
+//        )
+//            .background(
+//                Circle().fill(
+//                    LinearGradient(colors: [Color.blue,Color.green], startPoint:.leading, endPoint:.trailing)
+//                ).frame(width: 120,height: 120)
+//            )
+        RoundedRectangle(cornerRadius: 24).fill(AppColors.button).frame(width: .infinity,height: 56).padding(.all).overlay(Text("Login").font(
+            .system(size: 24,weight: .bold,design: .rounded)))
     }
 }
 

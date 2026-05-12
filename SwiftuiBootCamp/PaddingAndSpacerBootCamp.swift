87649e095d2e6be80b90b1a338f1b360a4befab9
ba138 <basitalyshah51214@gmail.com>
2026-05-12 10:55:40 +0500

    Adding padding to the text

---
+++ SwiftuiBootCamp/PaddingAndSpacerBootCamp.swift
@@ -0,0 +1,22 @@
+//
+//  PaddingAndSpacerBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 12/05/2026.
+//
+
+import SwiftUI
+
+struct PaddingAndSpacerBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding(.all,30)
+            .foregroundColor(.white     )
+            .font(.system(size: 30))
+            .background(.red)
+    }
+}
+
+#Preview {
+    PaddingAndSpacerBootCamp()
+}

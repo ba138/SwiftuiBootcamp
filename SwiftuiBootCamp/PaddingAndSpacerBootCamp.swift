e806d2dc09308eb7449d2c40b519ec8c9f0d28a9
ba138 <basitalyshah51214@gmail.com>
2026-05-12 11:13:15 +0500

    Adding padding to the vstack and add multiple Texts to it

--- SwiftuiBootCamp/PaddingAndSpacerBootCamp.swift
+++ SwiftuiBootCamp/PaddingAndSpacerBootCamp.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 struct PaddingAndSpacerBootCamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .padding(.all,30)
-            .padding(.leading,20)
-            .foregroundColor(.white     )
-            .font(.system(size: 30))
-            .background(.red)
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .padding(.all,30)
+//            .padding(.leading,20)
+//            .foregroundColor(.white     )
+//            .font(.system(size: 30))
+//            .background(.red)
+        VStack(
+            alignment: .leading, 
+            content: {
+            Text("This is Padding")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                
+                Text("This is the discription text of the padding which should be align to the leading and it should work because i use leading ")            }).padding(.all,8)
+.background(
+                Color.blue
+            )
+        
+        
     }
 }
 

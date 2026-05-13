b7c6683a0ee15523753378a7ace4feecee89a18f
ba138 <basitalyshah51214@gmail.com>
2026-05-13 12:58:15 +0500

    Adding spacer boot camp file to the project

---
+++ SwiftuiBootCamp/SpacersBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  SpacersBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 13/05/2026.
+//
+
+import SwiftUI
+
+struct SpacersBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpacersBootCamp()
+}

8d59e4c1eff94d94300053f776d50cd001276eeb
ba138 <basitalyshah51214@gmail.com>
2026-05-13 20:00:46 +0500

    Adding variable to the body so that i will use in the view

--- SwiftuiBootCamp/InitizerandEnumsBootCamp.swift
+++ SwiftuiBootCamp/InitizerandEnumsBootCamp.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct InitizerandEnumsBootCamp: View {
+    let backgroundColor: Color = Color.red
     var body: some View {
         VStack{
             Text("5")
             Text("Apples")
         }
         .frame(width: 200,height: 200)
-        .background(.red)
+        .background(backgroundColor)
         .cornerRadius(20)
         .font(.system(size: 30,))
         .foregroundColor(.white)

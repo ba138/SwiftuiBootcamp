6c09d76a3cfa274613b73cd06e1f4d565685079f
ba138 <basitalyshah51214@gmail.com>
2026-05-14 15:28:04 +0500

    Adding for ecah loop in vstack

---
+++ SwiftuiBootCamp/ForEachBootCamp.swift
@@ -0,0 +1,22 @@
+//
+//  ForEachBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 14/05/2026.
+//
+
+import SwiftUI
+
+struct ForEachBootCamp: View {
+    var body: some View {
+        VStack{
+            ForEach(0..<10, content: {index in
+            Text("Hi")
+            })
+        }
+    }
+}
+
+#Preview {
+    ForEachBootCamp()
+}

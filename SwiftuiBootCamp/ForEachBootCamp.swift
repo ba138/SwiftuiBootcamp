785c7de1deb5fda6003eadfad3dce0e52a086c34
ba138 <basitalyshah51214@gmail.com>
2026-05-14 15:45:41 +0500

    set loop in data set and show data in list

--- SwiftuiBootCamp/ForEachBootCamp.swift
+++ SwiftuiBootCamp/ForEachBootCamp.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct ForEachBootCamp: View {
+    let data: [String] = [
+        "hi there",
+        "hello",
+        "nice",
+        "well",
+    ]
     var body: some View {
-        VStack{
-            ForEach(0..<10, content: {index in
-//            Text("\(index)")
-                HStack{
-                    Circle()
-                        .fill(.red)
-                        .frame(width: 50)
-                    
-                    Text("Index : \(index)").font(.system(size: 24))
-                }
-            })
+        
+        ForEach(data.indices){index in
+            Text("Greetings : \(data[index])")
+            
         }
+//        VStack{
+//            ForEach(0..<10, content: {index in
+////            Text("\(index)")
+//                HStack{
+//                    Circle()
+//                        .fill(.red)
+//                        .frame(width: 50)
+//                    
+//                    Text("Index : \(index)").font(.system(size: 24))
+//                }
+//            })
+//        }
     }
 }
 

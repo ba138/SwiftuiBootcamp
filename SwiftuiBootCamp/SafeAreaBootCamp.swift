4e1133c97a3321794458b0c320ebcac15d715abe
ba138 <basitalyshah51214@gmail.com>
2026-05-17 11:36:15 +0500

    Adding safearea inside the zstack for background

--- SwiftuiBootCamp/SafeAreaBootCamp.swift
+++ SwiftuiBootCamp/SafeAreaBootCamp.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct SafeAreaBootCamp: View {
     var body: some View {
         ZStack {
+            //Background
+            Color.green.edgesIgnoringSafeArea(.
+            all)
+            //Foreground
             VStack {
                 Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 Spacer()
             }                .frame(maxWidth: .infinity,maxHeight: .infinity)
-                .background(.red)
-                .edgesIgnoringSafeArea(.top)
+//                .background(.red)
+                
         }
 
     }

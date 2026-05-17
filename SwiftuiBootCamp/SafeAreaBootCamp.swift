5d413feabdcba6483d52188be6961f737d098000
ba138 <basitalyshah51214@gmail.com>
2026-05-17 11:29:42 +0500

    Adding safe area to the text

---
+++ SwiftuiBootCamp/SafeAreaBootCamp.swift
@@ -0,0 +1,23 @@
+//
+//  SafeAreaBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 17/05/2026.
+//
+
+import SwiftUI
+
+struct SafeAreaBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .frame(maxWidth: .infinity,maxHeight: .infinity)
+            .background(.red)
+            .edgesIgnoringSafeArea(Edge.Set.top
+            
+            )
+    }
+}
+
+#Preview {
+    SafeAreaBootCamp()
+}

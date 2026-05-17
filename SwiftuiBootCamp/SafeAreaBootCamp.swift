3cc20de6cf53005c9677dea47e5222858bb30aa9
ba138 <basitalyshah51214@gmail.com>
2026-05-17 11:45:46 +0500

    Adding safea area to the scrollview

--- SwiftuiBootCamp/SafeAreaBootCamp.swift
+++ SwiftuiBootCamp/SafeAreaBootCamp.swift
@@ -9,18 +9,32 @@ import SwiftUI
 
 struct SafeAreaBootCamp: View {
     var body: some View {
-        ZStack {
-            //Background
-            Color.green.edgesIgnoringSafeArea(.
-            all)
-            //Foreground
-            VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                Spacer()
-            }                .frame(maxWidth: .infinity,maxHeight: .infinity)
-//                .background(.red)
-                
-        }
+//        ZStack {
+//            //Background
+//            Color.green.edgesIgnoringSafeArea(.
+//            all)
+//            //Foreground
+//            VStack {
+//                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//                Spacer()
+//            }                .frame(maxWidth: .infinity,maxHeight: .infinity)
+////                .background(.red)
+//
+//        }
+        ScrollView{
+            VStack{
+                Text(" scrollview")
+                    .font(.largeTitle,)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                ForEach(0..<20) { index in
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(.white)
+                        .shadow(radius: 10)
+                        .frame(width: 300,height: 300)
+                        .padding(20)
+                }
+            }
+        }.edgesIgnoringSafeArea(.all).background(.red)
 
     }
 }

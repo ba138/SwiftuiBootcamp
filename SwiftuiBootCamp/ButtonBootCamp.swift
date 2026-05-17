4949cb6cc417679cad3d0c11fd75675b79d7cd85
ba138 <basitalyshah51214@gmail.com>
2026-05-17 19:47:07 +0500

    Adding button to button boot camp

---
+++ SwiftuiBootCamp/ButtonBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 17/05/2026.
+//
+
+import SwiftUI
+
+struct ButtonBootCamp: View {
+    var body: some View {
+        Button("Press"){
+            
+        }
+    }
+}
+
+#Preview {
+    ButtonBootCamp()
+}

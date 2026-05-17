bae72715242f97b9bac828b6abcfcf03df71ce2d
ba138 <basitalyshah51214@gmail.com>
2026-05-17 19:52:46 +0500

    Add the state variable to button boot camp

--- SwiftuiBootCamp/ButtonBootCamp.swift
+++ SwiftuiBootCamp/ButtonBootCamp.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ButtonBootCamp: View {
+    @State var title: String = "Press the button to change the title"
     var body: some View {
-        Button("Press"){
-            
+        VStack(spacing:20) {
+            Text(title)
+            Button("Press"){
+                self.title = "Button was pressed"
+            }
         }
     }
 }

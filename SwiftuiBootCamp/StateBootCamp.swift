fa9ff02e705d26b3a7e8d1975a92fa246e2550bd
ba138 <basitalyshah51214@gmail.com>
2026-05-18 11:24:45 +0500

    Add background layer and content to the state boot camp

--- SwiftuiBootCamp/StateBootCamp.swift
+++ SwiftuiBootCamp/StateBootCamp.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct StateBootCamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            // background
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+                // content layer
+            VStack(spacing: 20,) {
+                Text("Title of the layer")
+                    .font(
+                        .system(
+                            size: 30,
+                            weight: .bold
+                        )
+                    )
+                    .foregroundColor(
+                        .white
+                    )
+                Text("Count : 1")
+                    .font(
+                        .system(
+                            size: 30,
+                            weight: .bold
+                        )
+                    )
+                    .foregroundColor(
+                        .white
+                    )
+                HStack(spacing:20) {
+                    Button {
+                        
+                    } label: {
+                        Text("Button 1")
+                            .foregroundColor(.black)
+                            .padding()
+                            .padding(.horizontal,20)
+                            .background(
+                                .white
+                            )
+                            .cornerRadius(10)
+                    }
+                    Button {
+                        
+                    } label: {
+                        Text("Button 2")
+                            .foregroundColor(.black)
+                            .padding()
+                            .padding(.horizontal,20)
+                            .background(
+                                .white
+                            )
+                            .cornerRadius(10)
+                    }
+
+
+                }
+            }
+        }
     }
 }
 

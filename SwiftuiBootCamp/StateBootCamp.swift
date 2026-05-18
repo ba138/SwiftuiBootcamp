2c5dcbb6aa7aff95487ca6ddcaac5f6b4a94d3a4
ba138 <basitalyshah51214@gmail.com>
2026-05-18 11:15:01 +0500

    Add a new file for state called state boot camp

---
+++ SwiftuiBootCamp/StateBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  StateBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 18/05/2026.
+//
+
+import SwiftUI
+
+struct StateBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateBootCamp()
+}

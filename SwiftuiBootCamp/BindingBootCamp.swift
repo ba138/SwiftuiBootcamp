40930c6e2039919ea0e8068cdd8eb843968abf44
ba138 <basitalyshah51214@gmail.com>
2026-05-21 11:28:30 +0500

    pass count to subview from parent view using binding and state

--- SwiftuiBootCamp/BindingBootCamp.swift
+++ SwiftuiBootCamp/BindingBootCamp.swift
@@ -9,27 +9,36 @@ import SwiftUI
 
 struct BindingBootCamp: View {
     @State var bgColor : Color = Color.green
+    @State var count : Int = 0
     var body: some View {
         ZStack{
             bgColor
                 .ignoresSafeArea()
-            ButtonView(bgColor: $bgColor)
+            VStack{
+                Text("Count : \(count)")
+                ButtonView(bgColor: $bgColor,count: $count)
+
+            }
         }
     }
 }
 struct ButtonView : View {
     @Binding var bgColor : Color
+    @State var buttonColor : Color = Color.blue
+    @Binding var count : Int
     var body: some View {
         VStack{
             Button {
                 bgColor = .yellow
+                buttonColor = .pink
+                count += 1
             } label: {
                 Text("Button")
                     .foregroundColor(.white)
                     .padding()
                     .padding(.horizontal)
                     .background(
-                        .blue
+                        buttonColor
                     )
                     .cornerRadius(10)
             }

1e14da684b384f424be939fc74da4678f0a06b26
ba138 <basitalyshah51214@gmail.com>
2026-05-22 11:54:54 +0500

    Adding loading indicator use conditional statements

--- SwiftuiBootCamp/ConditionalBootCamp.swift
+++ SwiftuiBootCamp/ConditionalBootCamp.swift
@@ -10,50 +10,61 @@ import SwiftUI
 struct ConditionalBootCamp: View {
     @State var isShow: Bool = false;
     @State var shoeRectangle : Bool = false;
+    @State var isLoading : Bool = false;
     var body: some View {
-        ScrollView {
-            VStack(spacing:20){
-                Button {
-                    isShow.toggle()
-                } label: {
-                    Text("Pressed : \(isShow.description)")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.red)
-                        .cornerRadius(6)
-                }
-                Button {
-                    shoeRectangle.toggle()
-                } label: {
-                    Text("Rectangle : \(shoeRectangle.description)")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.red)
-                        .cornerRadius(6)
-                }
-                
-                if isShow{
-                    Circle(
-                        
-                    )
-                    .fill(.brown)
-                    .padding(.all,40)
-                    
-                }
-                if shoeRectangle{
-                    Rectangle()
-                        .fill(.gray)
-                        .frame(width: 200,height: 200)
-                        .padding(.all,40)
-                }
-                if !isShow || shoeRectangle{
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(width: 200,height: 300)
-                }
-                Spacer()
-                
-            }
+        
+        Button {
+            isLoading.toggle()
+        } label: {
+            Text("IsLoading : \(isLoading.description)")
         }
+        if isLoading {
+            ProgressView()
+        }
+
+//        ScrollView {
+//            VStack(spacing:20){
+//                Button {
+//                    isShow.toggle()
+//                } label: {
+//                    Text("Pressed : \(isShow.description)")
+//                        .foregroundColor(.white)
+//                        .padding()
+//                        .background(Color.red)
+//                        .cornerRadius(6)
+//                }
+//                Button {
+//                    shoeRectangle.toggle()
+//                } label: {
+//                    Text("Rectangle : \(shoeRectangle.description)")
+//                        .foregroundColor(.white)
+//                        .padding()
+//                        .background(Color.red)
+//                        .cornerRadius(6)
+//                }
+//                
+//                if isShow{
+//                    Circle(
+//                        
+//                    )
+//                    .fill(.brown)
+//                    .padding(.all,40)
+//                    
+//                }
+//                if shoeRectangle{
+//                    Rectangle()
+//                        .fill(.gray)
+//                        .frame(width: 200,height: 200)
+//                        .padding(.all,40)
+//                }
+//                if !isShow || shoeRectangle{
+//                    RoundedRectangle(cornerRadius: 20)
+//                        .frame(width: 200,height: 300)
+//                }
+//                Spacer()
+//                
+//            }
+//        }
     }
 }
 

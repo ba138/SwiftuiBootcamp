d30cbb41f79f0346c0924cc379f5644c62afda40
ba138 <basitalyshah51214@gmail.com>
2026-05-23 12:12:52 +0500

    Add content to ternary file

---
+++ SwiftuiBootCamp/TernaryOpetrosBootCamp.swift
@@ -0,0 +1,26 @@
+//
+//  TernaryOpetrosBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 23/05/2026.
+//
+
+import SwiftUI
+
+struct TernaryOpetrosBootCamp: View {
+    var body: some View {
+VStack{
+          
+    Button("Button"){
+        
+    }
+    RoundedRectangle(cornerRadius: 25)
+        .fill(.red)
+        .frame(width: 200,height: 100)
+        }
+    }
+}
+
+#Preview {
+    TernaryOpetrosBootCamp()
+}

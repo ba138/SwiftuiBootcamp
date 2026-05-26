bcd6479d2ae15e5cf164cd226b86a4a1fb4a3f53
ba138 <basitalyshah51214@gmail.com>
2026-05-26 20:19:17 +0500

    Add section in the list and add data on it

--- SwiftuiBootCamp/ListBootCamp.swift
+++ SwiftuiBootCamp/ListBootCamp.swift
@@ -14,10 +14,45 @@ struct ListBootCamp: View {
        "Orange",
        "Peach"
     ]
+    @State var vegatables : [String] = [
+        "ladyFinger",
+        "Onion",
+        "Patatos",
+        "Tamottaos"
+    ]
     
     var body: some View {
         List{
-            
+            Section {
+                ForEach(fruits, id:\.self) { index in
+                    Text(index.capitalized)
+                }
+            } header: {
+                Text("Fruits")
+                    .foregroundColor(.black)
+                    .font(
+                        .system(
+                       size: 24,
+                       weight: .bold
+                    )
+                    
+                    )
+            }
+            Section {
+                ForEach(vegatables, id:\.self) { index in
+                    Text(index.capitalized)
+                }
+            } header: {
+                Text("Vegatables")
+                    .foregroundColor(.black)
+                    .font(
+                        .system(
+                       size: 24,
+                       weight: .bold
+                    )
+                    
+                    )
+            }
         }
     }
 }

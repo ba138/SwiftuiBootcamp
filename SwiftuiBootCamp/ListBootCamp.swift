fe573083edef739b46802036b62d8208902ed4be
ba138 <basitalyshah51214@gmail.com>
2026-05-26 19:45:29 +0500

    add a lsit to the body

---
+++ SwiftuiBootCamp/ListBootCamp.swift
@@ -0,0 +1,27 @@
+//
+//  ListBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 26/05/2026.
+//
+
+import SwiftUI
+
+struct ListBootCamp: View {
+    @State var fruits : [String] = [
+       "Apple",
+       "Banana",
+       "Orange",
+       "Peach"
+    ]
+    
+    var body: some View {
+        List{
+            
+        }
+    }
+}
+
+#Preview {
+    ListBootCamp()
+}

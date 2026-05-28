d4f51a6085b0106c3fb327991c7e6df0e2e5714e
ba138 <basitalyshah51214@gmail.com>
2026-05-28 12:47:45 +0500

    add group list style to the list

--- SwiftuiBootCamp/ListBootCamp.swift
+++ SwiftuiBootCamp/ListBootCamp.swift
@@ -62,6 +62,7 @@ struct ListBootCamp: View {
                
 
             }
+            .listStyle(GroupedListStyle())
             .navigationTitle(Text("Grocery List"),
             ).navigationBarItems(
                 leading: EditButton(),

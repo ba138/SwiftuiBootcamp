89346a6ff51d3ff8094e6883fe5a3d4a6c191661
ba138 <basitalyshah51214@gmail.com>
2026-05-29 12:29:51 +0500

    add dynamic content to the alert

--- SwiftuiBootCamp/AlertBootCamp.swift
+++ SwiftuiBootCamp/AlertBootCamp.swift
@@ -8,30 +8,49 @@
 import SwiftUI
 
 struct AlertBootCamp: View {
+    @State var title : String = ""
+    @State var message : String = ""
     @State var showAlert : Bool = false
     @State var bgColor : Color = .yellow
     var body: some View {
         ZStack {
             bgColor.ignoresSafeArea()
-            Button {
-                showAlert.toggle()
-            } label: {
-                Text("Click Here")
+            HStack {
+                Button {
+                    showAlert.toggle()
+                    title = "Failed to upload video".capitalized
+                    message = "Error occure during uploading".capitalized
+                } label: {
+                    Text("Click Here 1")
+                }
+                Button {
+                    showAlert.toggle()
+                    title = "Uploading Success".capitalized
+                    message = "Your video is uploaded suceesfuly🥳".capitalized
+                } label: {
+                    Text("Click Here 2")
+                }
             }.alert(isPresented: $showAlert, content: {
                 getAlert()
             })
+
         }
 
     }
     func getAlert() -> Alert {
         return Alert(
-            title: Text("Error"),
-            message: Text("This is th error"),
-            primaryButton: .destructive(Text("Delete"),action: {
-                bgColor = .white
-            }), secondaryButton: .cancel()
-            
+            title: Text(title),
+            message: Text(message),
+            dismissButton: .default(Text("Okay"))
         )
+//        return Alert(
+//            title: Text("Error"),
+//            message: Text("This is th error"),
+//            primaryButton: .destructive(Text("Delete"),action: {
+//                bgColor = .white
+//            }), secondaryButton: .cancel()
+//            
+//        )
     }
 }
 

ee7341379b2d801deba74cff801d761f7c929012
ba138 <basitalyshah51214@gmail.com>
2026-05-29 12:12:57 +0500

    add delete function to destructive button in the alert

--- SwiftuiBootCamp/AlertBootCamp.swift
+++ SwiftuiBootCamp/AlertBootCamp.swift
@@ -9,19 +9,25 @@ import SwiftUI
 
 struct AlertBootCamp: View {
     @State var showAlert : Bool = false
+    @State var bgColor : Color = .yellow
     var body: some View {
-        Button {
-            showAlert.toggle()
-        } label: {
-            Text("Click Here")
-        }.alert(isPresented: $showAlert) {
-//            Alert(title: Text("Error"))
-            Alert(
-                title: Text("Error"),
-                  message: Text("This is th error"),
-                primaryButton: .destructive(Text("Delete")), secondaryButton: .cancel()
-                
-            )
+        ZStack {
+            bgColor.ignoresSafeArea()
+            Button {
+                showAlert.toggle()
+            } label: {
+                Text("Click Here")
+            }.alert(isPresented: $showAlert) {
+                //            Alert(title: Text("Error"))
+                Alert(
+                    title: Text("Error"),
+                    message: Text("This is th error"),
+                    primaryButton: .destructive(Text("Delete"),action: {
+                        bgColor = .white
+                    }), secondaryButton: .cancel()
+                    
+                )
+            }
         }
 
     }

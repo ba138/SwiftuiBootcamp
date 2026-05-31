ac7c946e2e38e4d47090b6f3722766d4f7be1a7a
ba138 <basitalyshah51214@gmail.com>
2026-05-31 10:58:54 +0500

    design a login screen usong textfield image text and button

--- SwiftuiBootCamp/TextFieldBootCamp.swift
+++ SwiftuiBootCamp/TextFieldBootCamp.swift
@@ -9,8 +9,62 @@ import SwiftUI
 
 struct TextFieldBootCamp: View {
     @State var textFieldText : String = ""
+    @State var password : String = ""
     var body: some View {
-TextField("Search here",text: $textFieldText)
+        VStack(alignment : .leading){
+            Image(systemName: "person.fill")
+                .font(.system(size: 100))
+                .frame(maxWidth: .infinity, alignment: .center)
+            Spacer()
+            Text("Welocme Back please use you cred to login to your account")
+
+                .font(.system(size: 24))
+            Spacer()
+
+            Text("Email")
+            TextField("Enter your Email",text: $textFieldText)
+                .padding()
+
+                .background(Color.gray.opacity(0.3).cornerRadius(10))
+            Text("Password")
+            TextField("Enter your Password",text: $password
+            )
+                .padding()
+
+                .background(Color.gray.opacity(0.3).cornerRadius(10))
+            Button {
+                
+            } label: {
+                VStack(alignment :.trailing ) {
+                    Text("Forget Password?")
+                }
+            }.frame(maxWidth: .infinity, alignment: .trailing)
+
+            Spacer()
+            Button {
+                
+            } label: {
+Text("Login")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        Color.green
+                            .cornerRadius(10.0)
+                    )
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+            }
+            Spacer()
+            HStack {
+                Text("Already have account?".capitalized)
+                Text("Register")
+                    .font(.system(size: 18,weight: .bold))
+            }
+            .frame(maxWidth: .infinity)
+
+
+        }
+        .padding()
     }
 }
 

4c67cc58534b149fe84f68f2fed8a3d3d610e137
ba138 <basitalyshah51214@gmail.com>
2026-06-01 10:40:55 +0500

    add status using toggle and stae variable

--- SwiftuiBootCamp/ToggleBootCamp.swift
+++ SwiftuiBootCamp/ToggleBootCamp.swift
@@ -10,11 +10,19 @@ import SwiftUI
 struct ToggleBootCamp: View {
     @State var isToggle : Bool = false
     var body: some View {
-        Toggle(isOn: $isToggle) {
-            Text("Notifications")
+        VStack {
+            HStack{
+                Text("Status :")
+                Text(isToggle ? "Online" : "Offline")
+                    .foregroundColor(isToggle ? .green : .gray)
+            }
+            Toggle(isOn: $isToggle) {
+                Text("Notifications")
+            }
+            .toggleStyle(SwitchToggleStyle(tint:Color.red))
+            .padding()
+            Spacer()
         }
-        .toggleStyle(SwitchToggleStyle(tint:Color.red))
-        .padding()
     }
 }
 

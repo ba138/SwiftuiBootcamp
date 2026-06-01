774ca233694a4ad8429cfdd28ab2ba303bec6a00
ba138 <basitalyshah51214@gmail.com>
2026-06-01 10:53:28 +0500

    add a foreach loop in the picker

--- SwiftuiBootCamp/PickerBootCamp.swift
+++ SwiftuiBootCamp/PickerBootCamp.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct PickerBootCamp: View {
-    @State var slectedText : String = "1"
+    @State var slectedText : String = "18"
     var body: some View {
         VStack {
             Picker(selection: $slectedText,
                    label: Text("Label"),
                    content:     {
-                Text("1").tag("1")
-                Text("2").tag("2")
-                
-                Text("3").tag("3")
-                
-                Text("4").tag("4")
-                
-                Text("5").tag("5")
+//                Text("1").tag("1")
+//                Text("2").tag("2")
+//                
+//                Text("3").tag("3")
+//                
+//                Text("4").tag("4")
+//                
+//                Text("5").tag("5")
+                ForEach(18..<100) { index in
+                    Text("\(index)").tag("\(index)")
+
+                }
                 
             }
             )

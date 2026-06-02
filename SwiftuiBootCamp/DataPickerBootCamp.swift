e5a9944d384f38ea70248d3c3cc7289f55612932
ba138 <basitalyshah51214@gmail.com>
2026-06-02 11:15:04 +0500

    add date picker boot camp to the project

---
+++ SwiftuiBootCamp/DataPickerBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  DataPickerBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 02/06/2026.
+//
+
+import SwiftUI
+
+struct DataPickerBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataPickerBootCamp()
+}

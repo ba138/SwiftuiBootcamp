371397f531251d7c47e2cf1b18ec539b2b8f91d6
ba138 <basitalyshah51214@gmail.com>
2026-06-03 11:09:03 +0500

    add a stepper to increment and decremnt width of the rounded rectangle

--- SwiftuiBootCamp/StepperBootCamp.swift
+++ SwiftuiBootCamp/StepperBootCamp.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct StepperBootCamp: View {
     @State var value : Int = 10
+    @State var widthValue : CGFloat = 0
     var body: some View {
         VStack {
             Text("\(value)")
             Stepper("Stepper",value: $value)
+            RoundedRectangle(cornerRadius: 25)
+                .frame(width:100 + widthValue,height: 100)
+            
+            Stepper("Stepper 2") {
+                widthValue += 10
+            }onDecrement: {
+               widthValue -= 10
+            }
+        
         } .padding()
     }
 }

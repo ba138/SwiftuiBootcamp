537040fd44aa404e4f6237e46fb5c71e986e3d16
ba138 <basitalyshah51214@gmail.com>
2026-06-04 15:41:41 +0500

    add tabview and it style is pageviewtabview

--- SwiftuiBootCamp/TabViewBootCamp.swift
+++ SwiftuiBootCamp/TabViewBootCamp.swift
@@ -10,27 +10,42 @@ import SwiftUI
 struct TabViewBootCamp: View {
     @State var selectedTab : Int = 0
     var body: some View {
+        TabView{
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(.red)
+                
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(.green)
 
-        TabView (selection : $selectedTab){
-HomeViewBootCamp(slectedTab: $selectedTab)
-            .tabItem{
-                    Image(systemName: "house.fill")
-                    Text("Home")
-                    
-            }.tag(0)
-DiscoverViewBootCamp()
-                .tabItem{
-                    Image(systemName: "globe.fill")
-                    Text("Discover")
-                    
-                }.tag(1)
-            SettingViewBootCamp()
-                .tabItem{
-                    Image(systemName: "person.fill")
-                    Text("Account")
-                    
-                }.tag(2)
+
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(.cyan)
+
+
         }
+        .frame(height: 400)
+        .padding()
+        .tabViewStyle(PageTabViewStyle())
+//        TabView (selection : $selectedTab){
+//HomeViewBootCamp(slectedTab: $selectedTab)
+//            .tabItem{
+//                    Image(systemName: "house.fill")
+//                    Text("Home")
+//                    
+//            }.tag(0)
+//DiscoverViewBootCamp()
+//                .tabItem{
+//                    Image(systemName: "globe.fill")
+//                    Text("Discover")
+//                    
+//                }.tag(1)
+//            SettingViewBootCamp()
+//                .tabItem{
+//                    Image(systemName: "person.fill")
+//                    Text("Account")
+//                    
+//                }.tag(2)
+//        }
     }
 }
 

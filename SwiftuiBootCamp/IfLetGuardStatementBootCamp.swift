cdce06130521e2d0ae812b214baceb47c15934d8
ba138 <basitalyshah51214@gmail.com>
2026-06-08 12:05:09 +0500

    add content for safe coding

---
+++ SwiftuiBootCamp/IfLetGuardStatementBootCamp.swift
@@ -0,0 +1,29 @@
+//
+//  IfLetGuardStatementBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 08/06/2026.
+//
+
+import SwiftUI
+
+struct IfLetGuardStatementBootCamp: View {
+    @State var displayText : String = ""
+    var body: some View {
+
+        NavigationView{
+            VStack{
+                Text("We are preticing Safe Coding!")
+                Text(displayText)
+                Spacer()
+            }
+            .navigationTitle(
+                Text("Safe Coding")
+            )
+        }
+    }
+}
+
+#Preview {
+    IfLetGuardStatementBootCamp()
+}

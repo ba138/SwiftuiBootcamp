634636a0805434a6688bfd3b49333dc555d9591a
ba138 <basitalyshah51214@gmail.com>
2026-06-09 11:52:50 +0500

    add user data to the ui

--- SwiftuiBootCamp/CustomeModelBootCamp.swift
+++ SwiftuiBootCamp/CustomeModelBootCamp.swift
@@ -6,30 +6,39 @@
 //
 
 import SwiftUI
-struct UserModel {
-    let userId : String = UUID()
-        .uuidString
-    let UserName : String
-    let phoneNumber : Int
-    let last name : String
-    
+struct UserModel: Identifiable {
+    let id: String = UUID().uuidString
+    let firstName: String
+    let lastName: String
+    let phoneNumber: String
 }
 
 struct CustomeModelBootCamp: View {
-    @State var users : [String] = [
-        "Basit",
-        "Touseef",
-        "Fayyaz",
-        "Atizaz"
+    @State var users : [UserModel] = [
+        UserModel(firstName: "Basit", lastName: "Ali", phoneNumber: "03160883492"),
+        UserModel(firstName: "Touseef", lastName: "Hussain", phoneNumber: "03165888595"),
+        UserModel(firstName: "Fayyaz", lastName: "Shah", phoneNumber: "123456789"),
+        UserModel(firstName: "Atizaz", lastName: "Shah", phoneNumber: "2345856")
     ]
     var body: some View {
         VStack{
-            List {
-                ForEach(users, id: \.self) { name in
-                    Text(name)
-                }
+            List(users) { user in
+                HStack{
+                    Circle()
+                        .frame(width: 50,height: 50)
+                    VStack(alignment: .leading) {
+                        
+                        Text("\(user.firstName) \(user.lastName)")
+                            .font(.headline)
+                        Text(user.phoneNumber)
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline)
+                    }
+                    }
+                    
             }
-        }    }
+        }
+    }
 }
 
 #Preview {

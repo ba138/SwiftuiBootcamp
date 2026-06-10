5f3c0a2db1a28b2c849b036d13c3ae75739711be
ba138 <basitalyshah51214@gmail.com>
2026-06-10 11:33:43 +0500

    add fruit model in the boot camp

---
+++ SwiftuiBootCamp/StateAndObesverableObjectBootCamp.swift
@@ -0,0 +1,24 @@
+//
+//  StateAndObesverableObjectBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 10/06/2026.
+//
+
+import SwiftUI
+struct FruitsModel : Identifiable {
+    let id : String = UUID().uuidString
+    let name : String
+    let count : Int
+}
+
+struct StateAndObesverableObjectBootCamp: View {
+    var body: some View {
+        List{
+            
+        }    }
+}
+
+#Preview {
+    StateAndObesverableObjectBootCamp()
+}

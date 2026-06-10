425b1698c711197e098da2c2b47e99fca236e1fd
ba138 <basitalyshah51214@gmail.com>
2026-06-10 11:45:27 +0500

    add new element to the arrey

--- SwiftuiBootCamp/StateAndObesverableObjectBootCamp.swift
+++ SwiftuiBootCamp/StateAndObesverableObjectBootCamp.swift
@@ -27,7 +27,22 @@ struct StateAndObesverableObjectBootCamp: View {
                         .bold()
                 }
             }
-        }    }
+        }.onAppear{
+            getFruits()
+        }
+    }
+    func getFruits()
+    {
+        let fruit1 = FruitsModel(name: "Grabes", count: 4)
+        let fruit2 = FruitsModel(name: "Tamoto", count: 3)
+        let fruit3 = FruitsModel(name: "Peaches", count: 12)
+        
+        fruitArray.append(fruit1)
+        fruitArray.append(fruit2)
+        fruitArray.append(fruit3)
+
+        
+    }
 }
 
 #Preview {

7562506a86cd1099dba0304345c367b5fffc920c
ba138 <basitalyshah51214@gmail.com>
2026-06-10 12:17:05 +0500

    add dispatch que in the fruit view model

---
+++ SwiftuiBootCamp/FruitViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  FruitViewModel.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 10/06/2026.
+//
+
+import SwiftUI
+import Combine
+
+class FruitsViewModel: ObservableObject {
+  @Published  var fruitArray : [FruitsModel] = [
+    ]
+    @Published var isLoading : Bool = false
+    func getFruits()
+    {
+        let fruit1 = FruitsModel(name: "Grabes", count: 4)
+        let fruit2 = FruitsModel(name: "Tamoto", count: 3)
+        let fruit3 = FruitsModel(name: "Peaches", count: 12)
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline:.now()+3) {
+            self.fruitArray.append(fruit1)
+            self.fruitArray.append(fruit2)
+            self.fruitArray.append(fruit3)
+            self.isLoading = false
+        }
+       
+
+        
+    }
+    func delete(index :IndexSet){
+        fruitArray.remove(atOffsets: index)
+    }
+}
--- SwiftuiBootCamp/StateAndObesverableObjectBootCamp.swift
+++ SwiftuiBootCamp/StateAndObesverableObjectBootCamp.swift
@@ -12,25 +12,7 @@ struct FruitsModel : Identifiable {
     let name : String
     let count : Int
 }
-class FruitsViewModel: ObservableObject {
-  @Published  var fruitArray : [FruitsModel] = [
-    ]
-    func getFruits()
-    {
-        let fruit1 = FruitsModel(name: "Grabes", count: 4)
-        let fruit2 = FruitsModel(name: "Tamoto", count: 3)
-        let fruit3 = FruitsModel(name: "Peaches", count: 12)
-        
-        fruitArray.append(fruit1)
-        fruitArray.append(fruit2)
-        fruitArray.append(fruit3)
 
-        
-    }
-    func delete(index :IndexSet){
-        fruitArray.remove(atOffsets: index)
-    }
-}
 
 struct StateAndObesverableObjectBootCamp: View {
 //    @State var fruitArray : [FruitsModel] = [

35467e316558ff53c5a972e8979655735b5f0838
ba138 <basitalyshah51214@gmail.com>
2026-06-12 11:25:22 +0500

    add user name by updating state variable

--- SwiftuiBootCamp/AppStorageBootCamp.swift
+++ SwiftuiBootCamp/AppStorageBootCamp.swift
@@ -11,11 +11,12 @@ struct AppStorageBootCamp: View {
     @State var currentUserNamw : String? = nil
     var body: some View {
         VStack{
+            Text(currentUserNamw ?? "Enter your name")
             if let name = currentUserNamw{
                 Text(name)
             }
             Button("save") {
-                
+                currentUserNamw = "Basit Ali"
             }
         }
     }

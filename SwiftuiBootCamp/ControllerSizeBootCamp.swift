e542825d045c3a51a2193bec878b38e49e270158
ba138 <basitalyshah51214@gmail.com>
2026-06-16 11:19:56 +0500

    add button to boot camp

---
+++ SwiftuiBootCamp/ControllerSizeBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ControllerSizeBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 16/06/2026.
+//
+
+import SwiftUI
+
+struct ControllerSizeBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControllerSizeBootCamp()
+}

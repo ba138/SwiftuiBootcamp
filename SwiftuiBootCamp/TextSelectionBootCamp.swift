0f8d15fb390db0e7f15df2c1937935b5060b0b37
ba138 <basitalyshah51214@gmail.com>
2026-06-16 11:13:12 +0500

    add text Selection

---
+++ SwiftuiBootCamp/TextSelectionBootCamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 16/06/2026.
+//
+
+import SwiftUI
+
+struct TextSelectionBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootCamp()
+}

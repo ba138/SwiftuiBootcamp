8996f96868bc3a2d362594bdbc08c905a58a708b
ba138 <basitalyshah51214@gmail.com>
2026-06-18 12:17:02 +0500

    ädd isfocued in the login forum

--- SwiftuiBootCamp/FocusStateBootCamp.swift
+++ SwiftuiBootCamp/FocusStateBootCamp.swift
@@ -8,17 +8,51 @@
 import SwiftUI
 
 struct FocusStateBootCamp: View {
+    @FocusState private var userFocus : Bool
     @State private var userName : String = ""
+    @FocusState private var passwordFocus : Bool
+    @State private var password : String = ""
     var body: some View {
         VStack{
             TextField("your Name here ...", text: $userName)
+                .focused($userFocus)
+                .padding(.leading)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.brightness(0.3))
+                .cornerRadius(12)
+            TextField("your Password here ...", text: $password)
+                .focused($passwordFocus)
                 .padding(.leading)
                 .frame(height: 55)
                 .frame(maxWidth: .infinity)
                 .background(Color.gray.brightness(0.3))
                 .cornerRadius(12)
 
+            Button {
+                let isNameValid = !userName.isEmpty
+                let isPasswordValid = !password.isEmpty
+                if isNameValid && isPasswordValid {
+                    print("Is login")
+                }
+                else if (isNameValid){
+                    userFocus = false
+                    passwordFocus = true
+                }
+                else{
+                    userFocus = true
+                    passwordFocus = false
+                }
+            } label: {
+                Text("Log In")
+                    .frame(height: 45)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.glassProminent)
+
+
         } .padding(20)
+            
             
     }
 }
--- SwiftuiBootCamp/SwiftuiBootCampApp.swift
+++ SwiftuiBootCamp/SwiftuiBootCampApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftuiBootCampApp: App {
     var body: some Scene {
         WindowGroup {
-            IntroView()
+            FocusStateBootCamp()
         }
     }
 }

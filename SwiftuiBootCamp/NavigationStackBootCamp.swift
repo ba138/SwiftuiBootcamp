e9b5fe3965319f9a6fc455c0982f2713629deded
ba138 <basitalyshah51214@gmail.com>
2026-06-19 12:49:17 +0500

    add second screen for the navigation stack

--- SwiftuiBootCamp/NavigationStackBootCamp.swift
+++ SwiftuiBootCamp/NavigationStackBootCamp.swift
@@ -11,12 +11,22 @@ struct NavigationStackBootCamp: View {
     var body: some View {
         NavigationStack{
             NavigationLink {
-                Text("Screen 2")
             } label: {
             Text("Click Me")
             }
             .navigationTitle("Navigation stack Boot Camp")
         }
+    }
+}
+struct ScreenTwo : View {
+    let value : Int
+    init(value: Int) {
+        self.value = value
+        print("value for screen \(value)")
+    }
+    var body: some View {
+        Text("Screen 2")
+
     }
 }
 

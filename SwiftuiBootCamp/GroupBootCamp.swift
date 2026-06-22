8273b13290346660968813ade45a2bd8c72a8135
ba138 <basitalyshah51214@gmail.com>
2026-06-22 12:30:48 +0500

    add content for group boot camp

---
+++ SwiftuiBootCamp/GroupBootCamp.swift
@@ -0,0 +1,24 @@
+//
+//  GroupBootCamp.swift
+//  SwiftuiBootCamp
+//
+//  Created by Basit Ali on 22/06/2026.
+//
+
+import SwiftUI
+
+struct GroupBootCamp: View {
+    var body: some View {
+        VStack (spacing : 50){
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+
+
+        }    }
+}
+
+#Preview {
+    GroupBootCamp()
+}

f4c603a7cfd0e8fb97ab1330a09749a0d6465c17
ba138 <basitalyshah51214@gmail.com>
2026-06-22 12:34:39 +0500

    ädd group modifer to the boot camp

--- SwiftuiBootCamp/GroupBootCamp.swift
+++ SwiftuiBootCamp/GroupBootCamp.swift
@@ -11,12 +11,19 @@ struct GroupBootCamp: View {
     var body: some View {
         VStack (spacing : 50){
             Text("Hello, World!")
-            Text("Hello, World!")
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Group {
+                Text("Hello, World!")
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .foregroundColor(.cyan)
+            .font(.title)
 
 
 
-        }    }
+        }
+        .foregroundColor(.green)
+        .font(.largeTitle)
+    }
 }
 
 #Preview {
